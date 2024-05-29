984a34c2a108cadccd56c657337eb3c8035a0e7d
Gabs343 <gabriellopezp343@gmail.com>
2024-05-29 16:37:47 -0300

    new wishlist mapper

---
+++ Mappers/WishlistMapper.cs
@@ -0,0 +1,15 @@
+﻿using ApiGames.Controllers.Responses;
+using ApiGames.Models;
+
+namespace ApiGames.Mappers
+{
+    public static class WishlistMapper
+    {
+        public static WishlistResponse GetWishlistResponseFromWishlist(Wishlist wishlist) {
+            return new WishlistResponse {
+                Id = wishlist.Id,
+                Games = GameMapper.GetGamesResponseFromGames(wishlist.Games)
+            };
+        }
+    }
+}
