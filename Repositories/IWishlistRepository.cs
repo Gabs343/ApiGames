5c7c2c3730f0152c366f7bd81a1db315864d9453
Gabs343 <gabriellopezp343@gmail.com>
2024-05-29 16:38:42 -0300

    new wishlist repository

---
+++ Repositories/IWishlistRepository.cs
@@ -0,0 +1,9 @@
+﻿using ApiGames.Models;
+
+namespace ApiGames.Repositories {
+    public interface IWishlistRepository {
+        public Task<Wishlist?> FindById(long id);
+
+        public Task Save();
+    }
+}
---
+++ Repositories/WishlistRepository.cs
@@ -0,0 +1,24 @@
+﻿using ApiGames.Data;
+using ApiGames.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiGames.Repositories {
+    public class WishlistRepository : IWishlistRepository {
+
+        private readonly MyContext _context;
+
+        public WishlistRepository(MyContext context) {
+            _context = context;
+        }
+        public async Task<Wishlist?> FindById(long id) {
+            return await _context.wishlists
+                .Include(wsh => wsh.Games)
+                .ThenInclude(gms => gms.Tags)
+                .FirstOrDefaultAsync(wsh => wsh.Id == id);
+        }
+
+        public async Task Save() {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
