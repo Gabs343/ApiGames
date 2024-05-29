c818a29af4c85f9d3e4f43dd4fbef839389202ee
Gabs343 <gabriellopezp343@gmail.com>
2024-05-29 16:39:00 -0300

    new wishlist service

---
+++ Services/IWishlistService.cs
@@ -0,0 +1,11 @@
+﻿using ApiGames.Models;
+
+namespace ApiGames.Services {
+    public interface IWishlistService {
+        public Task<Wishlist> FindById(long id);
+
+        public Task<Wishlist> AddGamesToWishlist(Wishlist wishlist, List<long> gamesIds);
+
+        public Task<Wishlist> RemoveGamesFromWishlist(Wishlist wishlist, List<Game> games);
+    }
+}
---
+++ Services/WishlistService.cs
@@ -0,0 +1,35 @@
+﻿using ApiGames.Models;
+using ApiGames.Repositories;
+
+namespace ApiGames.Services {
+    public class WishlistService : IWishlistService {
+
+        private readonly IWishlistRepository _repository;
+        private readonly IGameService _gameService;
+
+        public WishlistService(IWishlistRepository repository,
+                               IGameService gameService) { 
+            _repository = repository;
+            _gameService = gameService;
+        }
+
+        public async Task<Wishlist> AddGamesToWishlist(Wishlist wishlist, List<long> gamesIds) {
+            List<Game> games = _gameService.FindByIds(gamesIds);
+            games.ForEach(wishlist.AddGame);
+            await _repository.Save();
+            return wishlist;
+        }
+
+        public async Task<Wishlist> RemoveGamesFromWishlist(Wishlist wishlist, List<Game> games) {
+            games.ForEach(wishlist.RemoveGame);
+            await _repository.Save();
+            return wishlist;
+        }
+
+        public async Task<Wishlist> FindById(long id) {
+            Wishlist? wishlist = await _repository.FindById(id);
+            if (wishlist != null) { return wishlist; }
+            throw new Exception("There is a problem in finding the wishlist");
+        }
+    }
+}
