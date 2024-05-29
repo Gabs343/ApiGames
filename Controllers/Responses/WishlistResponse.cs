95e36b2bc4f5616585e8c6dd8e811be78299cd6c
Gabs343 <gabriellopezp343@gmail.com>
2024-05-29 16:38:02 -0300

    new wishlist response

---
+++ Controllers/Responses/WishlistResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ApiGames.Controllers.Responses {
+    public class WishlistResponse {
+        public long Id { get; set; }
+        public List<GameResponse> Games { get; set; }
+    }
+}
