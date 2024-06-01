371832ccbd03f58a5d61bbb029135f13e8c43115
Gabs343 <gabriellopezp343@gmail.com>
2024-06-01 18:25:17 -0300

    New entity Review

---
+++ Models/Review.cs
@@ -0,0 +1,32 @@
+﻿namespace ApiGames.Models {
+    public class Review {
+        public long Id { get; set; }
+
+        public DateTime PostedAt { get; set; }
+
+        public string Message { get; set; }
+
+        public bool IsRecommended { get; set; }
+
+        public long UserId { get; set; }
+
+        public long GameId { get; set; }
+
+        public User User { get; set; }
+
+        public Game Game { get; set; }
+
+        public Review() { }
+
+        public Review(long id, DateTime postedAt, string message, bool isRecommended, User user, Game game) {
+            Id = id;
+            PostedAt = postedAt;
+            IsRecommended = isRecommended;
+            Message = message;
+            User = user;
+            Game = game;
+        }
+
+
+    }
+}
