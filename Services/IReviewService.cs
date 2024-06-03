9171ae37608bda71e8acb9444a61fafbee4f4f98
Gabs343 <gabriellopezp343@gmail.com>
2024-06-03 18:27:32 -0300

    Adding service for the review entity

---
+++ Services/IReviewService.cs
@@ -0,0 +1,9 @@
+﻿using ApiGames.Models;
+
+namespace ApiGames.Services {
+    public interface IReviewService {
+        public List<Review> GetReviews();
+
+        public Task<Review> Save(Review review);
+    }
+}
---
+++ Services/ReviewService.cs
@@ -0,0 +1,49 @@
+﻿using ApiGames.Models;
+using ApiGames.Repositories;
+
+namespace ApiGames.Services {
+    public class ReviewService : IReviewService {
+
+        private readonly IReviewRepository _repository;
+        private readonly IUserService _userService;
+        private readonly IGameService _gameService;
+
+        public ReviewService(IReviewRepository repository,
+                            IUserService userService,
+                            IGameService gameService) {
+            _repository = repository;
+            _userService = userService;
+            _gameService = gameService;
+        }
+
+        public List<Review> GetReviews() {
+            return _repository.GetAll();
+        }
+
+        public async Task<Review> Save(Review review) {
+            Game game = await GetGameById(review.GameId);
+            User user = await GetUserById(review.UserId);
+
+            review = new Review { 
+                PostedAt = review.PostedAt,
+                IsRecommended = review.IsRecommended,
+                Message = review.Message,
+                UserId = user.Id,
+                GameId = game.Id,
+                User = user,
+                Game = game,
+            };
+ 
+            _repository.Insert(review);
+            return review;
+        }
+
+        private async Task<Game> GetGameById(long id) {
+            return await _gameService.FindById(id);
+        }
+
+        private async Task<User> GetUserById(long id) {
+            return await _userService.FindById(id);
+        }
+    }
+}
