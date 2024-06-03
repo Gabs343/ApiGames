36c9c946a6547c912f4009ddfe97ce50af1e28d9
Gabs343 <gabriellopezp343@gmail.com>
2024-06-03 18:26:05 -0300

    new Review Request

---
+++ Controllers/Requests/ReviewRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiGames.Controllers.Requests {
+    public class ReviewRequest {
+
+        public string Message { get; set; }
+
+        public bool IsRecommended { get; set; }
+
+        public long UserId {  get; set; }
+
+        public long GameId {  get; set; }
+    }
+}
