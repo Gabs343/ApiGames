f8f2a8102be923521ee71f5399e25db39041b64e
Gabs343 <gabriellopezp343@gmail.com>
2024-06-03 18:27:13 -0300

    Adding repository for the review entity

---
+++ Repositories/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApiGames.Models;
+
+namespace ApiGames.Repositories {
+    public interface IReviewRepository {
+
+        public List<Review> GetAll();
+
+        public void Insert(Review review);
+
+        public Task Save();
+    }
+}
---
+++ Repositories/ReviewRepository.cs
@@ -0,0 +1,25 @@
+﻿using ApiGames.Data;
+using ApiGames.Models;
+
+namespace ApiGames.Repositories {
+    public class ReviewRepository : IReviewRepository {
+
+        private readonly MyContext _context;
+
+        public ReviewRepository(MyContext context) {
+            _context = context;
+        }
+
+        public List<Review> GetAll() {
+            return _context.reviews.ToList();
+        }
+
+        public void Insert(Review review) {
+            _context.reviews.Add(review);
+        }
+
+        public async Task Save() {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
